9cf27c3d1e0aecfd485496aa5874c2435663d9d4
Artem Burlaka <guestartem@gmail.com>
2015-12-29 22:39:16 +0200

    Simple test with static names of files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by Artem on 29.12.2015.
@@ -6,14 +10,29 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String test = "123";
+        //для копирования файлов нужно писать путь не к директории, а именно к файлу!
+        //причем в dest тоже должен быть прописан путь к "еще не перемещенному" файлу
+        String src = "G:\\1t\\Main.java";
+        String dest = "G:\\2t\\Main.java";
 
-        System.out.println("Enter source folder: ");
-        String srcFolder = FolderChecker.checkFolderExist();
-        System.out.println("Enter destination folder: ");
-        String destFolder = FolderChecker.checkFolderExist();
+//        System.out.println("Enter source folder: ");
+//        String srcFolder = FolderChecker.checkFolderExist();
+//        System.out.println("Enter destination folder: ");
+//        String destFolder = FolderChecker.checkFolderExist();
+//
+//        System.out.println(srcFolder + " ||||| " + destFolder);
 
-        System.out.println(srcFolder + " ||||| " + destFolder);
+//        Path srcPath = Paths.get(srcFolder);
+//        Path destPath = Paths.get(destFolder);
+
+        //TODO прохождение по каждому файлу директории
+
+        Path srcPath = Paths.get(src);
+        Path destPath = Paths.get(dest);
+
+        Files.copy(srcPath, destPath);
+
+        System.out.println("Done!");
 
 
     }
